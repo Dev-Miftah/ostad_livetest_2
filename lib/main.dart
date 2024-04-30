6f51fbd2089d1b0a5a1a4590afe1bca60e363971
Miftah <miftahrahi24@gmail.com>
2024-04-30 23:33:41 +0600

    initial connit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ostad_livetest_2/screens/prifile_screen.dart';
+import 'package:ostad_livetest_2/screens/profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
